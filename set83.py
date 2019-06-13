5551db9030a446a9714b821699fdc8b318af2a66
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-06-13 14:39:39 +0530

    Create set83.py

---
+++ set83.py
@@ -0,0 +1,18 @@
+a1,b1,c1=input().split()
+a=int(a1)
+c=int(c1)
+if(b1=='+'):
+  d=a+c
+  print(d)
+if(b1=='-'):
+  d=a-c
+  print(d)
+if(b1=='*'):
+  d=a*c
+  print(d)
+if(b1=='/'):
+  d=a//c
+  print(d)
+if(b1=='%'):
+  d=a%c
+  print(d)
